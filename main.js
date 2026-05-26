46104ff10698f4666fc57374973ca9f7b67048f2
sagarkhunt55-debug <sagarkhunt55@gmail.com>
2026-05-26 16:24:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
@@ -15,31 +15,30 @@ function generateKey(id) {
         .toUpperCase();
 }
 
-const licenseFile = path.join(app.getPath('userData'), 'license.json');
+const licensePath = path.join(app.getPath('userData'), "license.json");
 
 function getSavedLicense() {
     try {
-        return JSON.parse(fs.readFileSync(licenseFile));
+        return JSON.parse(fs.readFileSync(licensePath));
     } catch {
         return null;
     }
 }
 
 function saveLicense(key) {
-    fs.writeFileSync(licenseFile, JSON.stringify({ key }));
+    fs.writeFileSync(licensePath, JSON.stringify({ key }));
 }
 
 function isValid(key) {
-    const deviceId = machineIdSync();
-    const correctKey = generateKey(deviceId);
-    return key === correctKey;
+    const id = machineIdSync();
+    const correct = generateKey(id);
+    return key === correct;
 }
 
-let mainWindow;
+let win;
 
-function createWindow(showLicense) {
-
-    mainWindow = new BrowserWindow({
+function createWindow(page) {
+    win = new BrowserWindow({
         width: 1400,
         height: 900,
         webPreferences: {
@@ -48,13 +47,8 @@ function createWindow(showLicense) {
         }
     });
 
-    if (showLicense) {
-        mainWindow.loadFile('license.html');
-    } else {
-        mainWindow.loadFile('GALAXY STONE REPORT.html');
-    }
-
-    mainWindow.removeMenu();
+    win.loadFile(page);
+    win.removeMenu();
 }
 
 app.whenReady().then(() => {
@@ -62,15 +56,15 @@ app.whenReady().then(() => {
     const saved = getSavedLicense();
 
     if (saved && isValid(saved.key)) {
-        createWindow(false);
+        createWindow("GALAXY STONE REPORT.html");
     } else {
-        createWindow(true);
+        createWindow("license.html");
     }
 
 });
 
-// IPC from license page
-ipcMain.on('validate-license', (event, key) => {
+// from license page
+ipcMain.on("check-license", (event, key) => {
 
     if (isValid(key)) {
 
@@ -80,12 +74,10 @@ ipcMain.on('validate-license', (event, key) => {
             message: "License Activated Successfully"
         });
 
-        mainWindow.loadFile('GALAXY STONE REPORT.html');
+        win.loadFile("GALAXY STONE REPORT.html");
 
     } else {
-
         dialog.showErrorBox("Error", "Invalid License Key");
-
     }
 
 });
