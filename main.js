fc4efee259f85b383b67795d1dd12eb6d5dece4f
sagarkhunt55-debug <sagarkhunt55@gmail.com>
2026-05-26 13:43:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow(){
+
+    const win = new BrowserWindow({
+
+        width:1400,
+        height:900
+
+    });
+
+    win.loadFile('index.html');
+
+    win.removeMenu();
+
+}
+
+app.whenReady().then(createWindow);
